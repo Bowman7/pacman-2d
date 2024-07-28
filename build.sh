57e1db624a8a187093d2cb4942dfa09019e61e6d
reraru505 <fnln504@gmail.com>
2024-07-28 18:15:40 +0530

    Build system improv

--- build.sh
+++ build.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-g++ main.cpp game.cpp grid2.cpp pac.cpp ghost.cpp -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -g
+
+
+if [ "$1" == "--debug" ];
+then
+    g++ *.cpp -Wall -Ihelper  -Iexternal -Lexternal  -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -g -o build/debug/pacman
+elif [ "$1" == "--release" ];
+then
+    g++ *.cpp -Ihelper  -Iexternal -Lexternal -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -O0 -o build/release/pacman
+elif [ "$1" == "--run" ];
+then
+    ./build/debug/pacman
+elif [ "$1" == "--cleanup" ];
+then
+    rm */**/*.cpp~
+    rm */**/*.hpp~
+    rm */**/*.sh~
+    rm *.cpp~
+    rm *.hpp~
+    rm *.sh~
+else
+    echo "code will be compiled in debug mode as none of the options was selected"
+    g++ *.cpp -Wall -Ihelper  -Iexternal -Lexternal  -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -g -o build/debug/pacman
+fi
+
+
